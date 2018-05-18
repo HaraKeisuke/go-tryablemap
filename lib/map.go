76e5d2e4b742999225a9a5308355339b9e72669a
HaraKeisuke <mtnnnnnnn@gmail.com>
2018-05-18 18:13:36 +0900

    tryablemap

--- lib/map.go
+++ lib/map.go
@@ -1,24 +1,35 @@
 package tryablemap
 
 type TryableMap struct {
-	Map map[string]interface{}
+	Map *map[string]interface{}
 }
 
 func NewTryableMap(_map map[string]interface{}) *TryableMap {
 	return &TryableMap{
-		Map: _map,
+		Map: &_map,
 	}
 }
 
 func (tmap *TryableMap) Try(key string) *TryableMap {
 	if tmap.Map == nil {
-		return nil
+		return &TryableMap{}
 	}
 
-	if val, ok := tmap.Map[key]; ok {
-		_val := val.(map[string]interface{})
+	_val, ok := (*tmap.Map)[key].(map[string]interface{})
+	if ok {
 		return &TryableMap{
-			Map: _val,
+			Map: &_val,
+		}
+	}
+	return &TryableMap{}
+}
+
+func (tmap *TryableMap) Value(key string) interface{} {
+	if tmap.Map != nil {
+		if val, ok := (*tmap.Map)[key]; ok {
+			return val
+		} else {
+			return nil
 		}
 	} else {
 		return nil
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-func main() {
-	// data := map[string]map[string]int{
-	// 	"first": map[string]int{},
-	// }
+import (
+	"fmt"
 
-	// fmt.Print("hoge")
+	tryablemap "github.com/HaraKeisuke/go-tryablemap/lib"
+)
+
+func main() {
+	data := map[string]interface{}{
+		"first": map[string]interface{}{
+			"second": 123,
+		},
+	}
+	_map := tryablemap.NewTryableMap(data)
+	result := _map.Try("first").Value("second")
+
+	fmt.Print(result)
 }
